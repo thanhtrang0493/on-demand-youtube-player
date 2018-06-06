7d7aed51a9502f2aa5843ca8308aa24f777f16ac
Ngo Thi Thanh Trang <ngo.thi.thanh.trang@styl.solutions>
2018-06-06 15:40:13 +0700

    login

--- app/src/main/java/com/vcoders/on_demand_youtube_player/features/myPlaylist/MyPlaylistPresenter.java
+++ app/src/main/java/com/vcoders/on_demand_youtube_player/features/myPlaylist/MyPlaylistPresenter.java
@@ -102,12 +102,7 @@ public class MyPlaylistPresenter extends BasePresenter<MyPlaylistView, MyPlaylis
 //        loginListener.onEvent(AuthRepo.this, AUTH_USER_AUTH_START);
 
         // may need to do this off UI thread?
-//        https://accounts.google.com/o/oauth2/v2/auth?
-//        scope=email%20profile&
-//                response_type=code&
-//                state=security_token%3D138r5719ru3e1%26url%3Dhttps://oauth2.example.com/token&
-//        redirect_uri=com.example.app:/oauth2redirect&
-//                client_id=client_id
+
         AuthorizationRequest.Builder authRequestBuilder = new AuthorizationRequest.Builder(
                 authState.getAuthorizationServiceConfiguration(),
                 Constant.CLIENT_ID,
--- app/src/main/java/com/vcoders/on_demand_youtube_player/utils/Constant.java
+++ app/src/main/java/com/vcoders/on_demand_youtube_player/utils/Constant.java
@@ -12,5 +12,6 @@ public class Constant {
     public static String BASE_URL_ACCOUNT = "https://accounts.google.com/o/oauth2/";
     public static String API_KEY = "AIzaSyBENTkoHFtO8tDHGC5Mcuzya9kT00kIusg";
     public static String CLIENT_ID = "230219820111-4sppgoniu3psv0hjdh0jc50hkf38jdca.apps.googleusercontent.com";
-    public static String REDIRECT_URI = "http://localhost/oauth2callback";
+//    public static String CLIENT_ID = "230219820111-j7td2ffbc9hrk1ec61qj88kqp2d2v030.apps.googleusercontent.com";
+    public static String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
 }
